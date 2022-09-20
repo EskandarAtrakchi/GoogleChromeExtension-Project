5a3f5af6b9c8385298fb37f40824eecaa54a9987
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-20 14:00:36 +0100

    content

--- content.js
+++ content.js
@@ -1,11 +1,1 @@
-var khaby = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuSyoMmDtRc4GL793HwpN7NatFeF-Cpp6v7iIwkY09OQ&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTcaYdHld2Yw9wkzs0zWkIYLvCYfFJ15J15UQ7dpHO6Q&s"
-];
-
-const imgs = document.getElementsByTagName("img")
-
-for (var I = 0; i < imgs.length; i++) {
-    const randomImg = Math.floor(Math.random () * khaby.length);
-    img[i].scr = khaby[randomImg];
-}
+//Any code can be implemented here 
