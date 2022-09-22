4848e9891321bbbf37dd7dd2d657edee68c4f699
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-22 22:02:15 +0100

    content

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-//Any code can be implemented here 
+//Any code can be implemented here
+/*
+!function(){var e=document.getElementsByTagName("script"),
+				t=e[e.length-1],n=document.createElement("script");
+				 function r(){var e=crCryptocoinPriceWidget.init({base:"USD,EUR",items:"GLQ,BTC,ETH,XRP,LTC,MCONTENT,BNB,ADA,SOL,AVAX,LUNA,DOT,DOGE,SHIB,CROATOM,LINK,TRX,NEAR,BCH,XLM,FTM,UNI,HBAR,MANA,SAND,ICP,CELR,VET,XMR,GRT,HNT,CAKE,ONE,ENJ,KSM,BAT,MINA,ZIL,KEEP",backgroundColor:"E0E0D9",streaming:"1",rounded:"1",boxShadow:"9",border:"5"});
+					t.parentNode.insertBefore(e,t)}n.src="https://co-in.io/widget/pricelist.js?items=BTC%2CETH%2CLTC%2CXMR%2CDASH",n.async=!0,n.readyState?n.onreadystatechange=function(){"loaded"!=n.readyState&&"complete"!=n.readyState||(n.onreadystatechange=null,r())}:n.onload=function(){r()},t.parentNode.insertBefore(n,null)}();
+*/
